c2a140976a09d8233d07c5fd2c3e20ba159832a7
Utilisateur <charlotte.amiot@gmail.com>
2021-05-15 17:58:23 +0200

    changement de style sur landing page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const txtAnim = document.querySelector('#txtAnim');
 new Typewriter(txtAnim, { deleteSpeed: 30 })
   .typeString(
-    '<strong id="nameAnim"><span class="jobAnim">Développeuse <span style="color:#0077b6">Front-End !</span></span></strong>'
+    '<strong id="nameAnim"><span class="jobAnim">Développeuse <span style="color:#4361EE">Front-End !</span></span></strong>'
   )
   .pauseFor(1000)
   .start();
